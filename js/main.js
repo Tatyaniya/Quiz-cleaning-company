18846dba84379f60a6b0d6d3226ca40fc0674f90
Tatyaniya <afromyak13@gmail.com>
2020-10-27 23:13:23 +0200

    js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const formItems = form.querySelectorAll('fieldset');
     const btnsNext = form.querySelectorAll('.form-button__btn-next');
     const btnsPrev = form.querySelectorAll('.form-button__btn-prev');
+    const answersObj = {
+        step0: {
+            question: '',
+            answers: [],
+        },
+        step1: {
+            question: '',
+            answers: [],
+        },
+        step2: {
+            question: '',
+            answers: [],
+        },
+        step3: {
+            question: '',
+            answers: [],
+        },
+        step4: {
+            name: '',
+            phone: '',
+            email: '',
+            call: '',
+        },
+    }
 
     btnsNext.forEach((btn, btnIndex) => {
         btn.addEventListener('click', (e) => {
@@ -15,16 +39,28 @@ document.addEventListener('DOMContentLoaded', function() {
             formItems[btnIndex].style.display = 'none';
             formItems[btnIndex + 1].style.display = 'block';
         });
+
+        btn.disabled = true;
     });
 
-    btnsPrev.forEach((btn, btnIndex) => {
-        btn.addEventListener('click', (e) => {
+    // btnsPrev.forEach((btn, btnIndex) => {
+    //     btn.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         // в блоке под индексом 1 находится кнопка 0, т.к. их на 1 меньше
+    //         formItems[btnIndex + 1].style.display = 'none';
+    //         formItems[btnIndex].style.display = 'block';
+    //     });
+    // });
+
+    // теперь тоже самое через for
+    for (let i = 0; i< btnsPrev.length; i++) {
+        btnsPrev[i].addEventListener('click', (e) => {
             e.preventDefault();
             // в блоке под индексом 1 находится кнопка 0, т.к. их на 1 меньше
-            formItems[btnIndex + 1].style.display = 'none';
-            formItems[btnIndex].style.display = 'block';
+            formItems[i + 1].style.display = 'none';
+            formItems[i].style.display = 'block';
         });
-    });
+    }
 
     formItems.forEach((formItem, formItemIndex) => {
 
@@ -37,39 +73,141 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // убираем все классы активности в 4-х филдсетах у родителей инпутов
         if(formItemIndex !== formItems.length - 1) {
+
             const inputs = formItem.querySelectorAll('input');
+            const itemTitle = formItem.querySelector('.form__title');
+
+            // при переборе филдсетов,  индекс будет меняться
+            answersObj[`step${formItemIndex}`].question = itemTitle.textContent;
             
             inputs.forEach((input) => {
                 const parent = input.parentNode;
                 
-                // сбросить чекед инпутов
+                // сбросить checked инпутов
                 input.checked = false;
                 parent.classList.remove('active-radio');
                 parent.classList.remove('active-checkbox');
             });
+
         }
 
         // выбор radio и checkbox
         formItem.addEventListener('change', (e) => {
-            const target = e.target;
 
-            if(target.classList.contains('form__radio')) {
-                console.log(target, 'radio');
-            } else if (target.classList.contains('form__input')) {
-                console.log(target, 'check');
-            } else {
-                return;
+            const target = e.target;
+            // находим все checked элементы
+            const inputsChecked = formItem.querySelectorAll('input:checked');
+
+            if(formItemIndex !== formItems.length - 1) {
+                // обнуляем ответы
+                answersObj[`step${formItemIndex}`].answers.length = 0;
+
+                // добавляем в массив все ответы на вопросы
+                inputsChecked.forEach(inputChecked => {
+                    answersObj[`step${formItemIndex}`].answers.push(inputChecked.value);
+                });
+
+                // если хоть что-то чекнуто
+                if(inputsChecked.length > 0) {
+                    // разблокировать именно эту кнопку
+                    btnsNext[formItemIndex].disabled = false;
+                } else {
+                // заблокировать
+                console.log(formItemIndex);
+                btnsNext[formItemIndex].disabled = true;
+                }
+
+                // если кликнули по радио-кнопке, добавить активный класс
+                if(target.classList.contains('form__radio')) {
+                
+                    const radioBtns = formItem.querySelectorAll('.form__radio');
+
+                    radioBtns.forEach(input => {
+                        if(input === target) {
+                            // add active class
+                            input.parentNode.classList.add('active-radio');
+                        } else {
+                            // remove active class
+                            input.parentNode.classList.remove('active-radio');
+                        }
+                    });
+                    
+                } else if (target.classList.contains('form__input')) {
+
+                    // если чекбоксы
+                    // target.parentNode.classList.toggle('active-checkbox');
+
+                    // если радио
+                    const checkBtns = formItem.querySelectorAll('.form__input');
+
+                    checkBtns.forEach(input => {
+                        if(input === target) {
+                            // add active class
+                            input.parentNode.classList.add('active-checkbox');
+                        } else {
+                            // remove active class
+                            input.parentNode.classList.remove('active-checkbox');
+                        }
+                    });
+
+                } else {
+                    return;
+                }
             }
+
         });
     });
 
-    overlay.style.display = 'none';
-    quiz.style.display = 'none';
+    const sendForm = () => {
+        const lastFeilset = formItems[formItems.length - 1];
+        
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            // сначала засунем данные в объект
+            answersObj.step4.name = document.getElementById('quiz-name').value;
+            answersObj.step4.phone = document.getElementById('quiz-phone').value;
+            answersObj.step4.email = document.getElementById('quiz-email').value;
+            answersObj.step4.call = document.getElementById('quiz-call').value;
+
+            // если чекнута обработка персональных данных
+            if (document.getElementById('quiz-policy').checked) {
+                postData(answersObj).then((res) => {
+                    if (res['status'] === 'ok') {
+                        overlay.style.display = 'none';
+                        quiz.style.display = 'none';
+                        form.reset();
+                        alert(res['message']);
+                    } else if (res['status'] === 'error') {
+                        alert(res['message']);
+                    }
+                });
+            } else {
+                alert('Дайте согласие на обработку персональных данных');
+            }
+
+        });
+    }
+
+    
+    const postData = (body) => {
+        return fetch('../server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+    }
+
+    overlay.style.display = "none";
+    quiz.style.display = "none";
 
     passTestButton.addEventListener('click', () => {
         overlay.style.display = 'block';
         quiz.style.display = 'block';
     });
 
+    sendForm();
    
 });
